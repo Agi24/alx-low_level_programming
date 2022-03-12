3cbc4b0720957890fea424c02085bf1cd3831478
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-03-11 18:48:23 -0800

    add print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - print all alphabets
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
