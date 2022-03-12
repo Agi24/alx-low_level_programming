ba21eb006e6650f8b68151542dfaddf162c92bf6
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-03-11 19:00:19 -0800

    implement print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - print all alphabets (lower and upper cases)
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
