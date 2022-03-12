18083439e57ad493ad168452eff0598614d26f4a
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-03-11 19:15:29 -0800

    implement print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print all numbers from 0 - 9
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+	return (0);
+}
