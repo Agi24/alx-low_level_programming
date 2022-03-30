a5cdb23e2c3c0a6eaf23a4bb5ff82fdf450d67c1
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-03-30 16:19:14 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,12 +7,12 @@
  */
 int _is_it_prm(int i, int n)
 {
-if (n < 2)
-	return (0);
+	if (n < 2)
+		return (0);
 	if (n % i == 0)
-	return (0);
+		return (0);
 	if (i > n / 2)
-	return (1);
+		return (1);
 	return (is_it_prm(i + 1, n));
 	}
 /**
